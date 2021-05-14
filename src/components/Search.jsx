247b739e913bdc263273b72a8e2b03d0c700d066
Karen Celis <k-celis@javeriana.edu.co>
2021-05-14 13:43:07 -0500

    footer template

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import '../assets/styles/components/Search.scss';
 
-
 const Search = () => (
-    <section className="main">
-    <h2 className="main__title">¿Qué quieres ver hoy?</h2>
-    <input type="text" classNAme="input" placeholder="Buscar..."/>
-    </section>
-)
+  <section className='main'>
+    <h2 className='main__title'>¿Qué quieres ver hoy?</h2>
+    <input type='text' className='input' placeholder='Buscar...' />
+  </section>
+);
 
 export default Search;
